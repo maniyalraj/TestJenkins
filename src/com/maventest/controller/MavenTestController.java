a61810ae4281a0de32b8f35097dae8fbbc3e54be
Raj <maniyalraj@gmail.com>
2016-09-28 15:32:25 +0530

    Controller, Index Page

--- src/com/maventest/controller/MavenTestController.java
+++ src/com/maventest/controller/MavenTestController.java
@@ -1,5 +1,15 @@
 package com.maventest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MavenTestController {
 
+	@RequestMapping(path="/")
+	public String getIndexPage()
+	{
+		return "index.html";
+	}
+	
 }
